a9d91220832b6d7ec58a0542e45eeccdef1afead
Kerollos90 <emadkero318@gmail.com>
2024-09-28 20:23:02 +0300

    Part 05 Product Specifications.

---
+++ Store.Repository/ProductSpecifications/ProductSpecification.cs
@@ -0,0 +1,31 @@
+﻿using Store.Data.Entites;
+using Store.Repository.Spcesifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.ProductSpecifications
+{
+    public class ProductSpecification : BaseSpecification<Product>
+    {
+        public ProductSpecification(BaseProductSpecif spec) : base
+            (
+               Product => (spec.BrandId.HasValue || Product.BrandId == spec.BrandId.Value) &&
+               (spec.TypeId.HasValue || Product.TypeId == spec.TypeId.Value)
+            )
+        {
+
+            AddIncludes(p=>p.BrandId);
+            AddIncludes(p=>p.TypeId);
+        
+        
+        }
+      
+
+
+
+
+    }
+}
