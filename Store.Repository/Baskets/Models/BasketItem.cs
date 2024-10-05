4a2b56c1808f41dd8e6be9b1522c94c6adcbd978
Kerollos90 <emadkero318@gmail.com>
2024-10-05 18:33:25 +0300

    Part 01 BasketRepository Setup.

---
+++ Store.Repository/Baskets/Models/BasketItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Store.Repository.Baskets.Models
+{
+    public class BasketItem
+    {
+        public  string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string BrandName { get; set; }
+
+        public string TypeName { get; set; }
+    }
+}
